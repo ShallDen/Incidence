2132f250211dd048c255574ee021d6394cbef542
Den Mayasov <shallden@yandex.ru>
2015-12-09 04:20:46 +0300

    Designer and icon files were added

---
+++ Incidence/Incidence/DictionaryForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Incidence
+{
+    partial class DictionaryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DictionaryForm));
+            this.dictionaryTextBox = new System.Windows.Forms.TextBox();
+            this.dictionaryLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dictionaryTextBox
+            // 
+            this.dictionaryTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dictionaryTextBox.Location = new System.Drawing.Point(23, 35);
+            this.dictionaryTextBox.Multiline = true;
+            this.dictionaryTextBox.Name = "dictionaryTextBox";
+            this.dictionaryTextBox.ReadOnly = true;
+            this.dictionaryTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.dictionaryTextBox.Size = new System.Drawing.Size(292, 398);
+            this.dictionaryTextBox.TabIndex = 2;
+            // 
+            // dictionaryLabel
+            // 
+            this.dictionaryLabel.AutoSize = true;
+            this.dictionaryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dictionaryLabel.Location = new System.Drawing.Point(20, 13);
+            this.dictionaryLabel.Name = "dictionaryLabel";
+            this.dictionaryLabel.Size = new System.Drawing.Size(57, 13);
+            this.dictionaryLabel.TabIndex = 1;
+            this.dictionaryLabel.Text = "Dictionary:";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(240, 443);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // DictionaryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 477);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.dictionaryLabel);
+            this.Controls.Add(this.dictionaryTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(354, 515);
+            this.Name = "DictionaryForm";
+            this.Text = "Dictionary";
+            this.Load += new System.EventHandler(this.DictionaryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dictionaryTextBox;
+        private System.Windows.Forms.Label dictionaryLabel;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
